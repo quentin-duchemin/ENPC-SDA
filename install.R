2971f241027577439d9eed0afdaeb52015f627e3
quentin-duchemin <quentin.duchemin@eleves.enpc.fr>
2021-08-06 16:05:43 +0200

    covid dataset done

---
+++ install.R
@@ -0,0 +1,12 @@
+install.packages("tidyverse")
+install.packages("car")
+install.packages('ggpubr')
+install.packages('ggplot2')
+install.packages('gplots')
+install.packages('purrr')
+install.packages('DescTools')
+install.packages('corrplot')
+install.packages('boot') # to use cv.glm for Cross-Validation
+install.packages('ROCR') # to use prediction and performance functions for ROC curves
+install.packages('gridExtra') # to use the command grid.arange to show multiple plots in the same window
+install.packages('lmtest') 
